6b8a1020d66d3d24365e03351e52571690351545
CoinTAEM <81678635+CoinTAEM@users.noreply.github.com>
2021-03-31 14:45:52 +0600

    test
    test

---
+++ configjg.js
@@ -0,0 +1,48 @@
+
+// Просто файл настройки бота, где можно указать основные параметры. Особо вдаваться в подробности, я не собираюсь
+
+
+module.exports = {
+
+  // Имя бота
+
+  botName:        `BapikBot`,
+
+  // Параметры группы
+
+    groupToken: '09e6ce52a78d27fd6e414ddca17628b322aa2f3c6878c5e40afd5053d7609b7384939580f823085ab1c97',
+    groupID: 180884630,
+
+  // VKCoin
+
+    vkcKey: `-f5nxPV6no7Ne0lfpUSBJ]ROY9e3ZlMpbabe;ataNgaK1*9uSN`,
+    vkcID: `625729206`,
+  
+  // PaperScroll
+    
+    psid: `625729206`,
+    pskey: `yOH5DLunF2f36PvJG04H0CnguAYQmhNJ0qioyubeE0mVjWlbcyv2s2RVDd13wapW`,
+
+  // CoronaCoin
+
+    cckey: `Y2rVsteG8Gwvpp65ce7XkmgyWWXk1KVe`,
+    ccid: `625729206`,
+
+  // GameCoin
+    gckey: `GtTxFsFqIU72odQtbVqEeAkj`,
+    gcid: `625729206`,
+
+
+  // База данных
+
+  urlDB:          'mongodb+srv://apmenlox1:apmenlox@cluster0.kjqmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  nameDB:         'apmenlox1',
+
+  // Награды за топ
+
+    PSTop: [2000000, 1000000, 500000, 250000, 150000, 100000, 50000, 50000, 50000, 50000],
+
+
+
+
+};
